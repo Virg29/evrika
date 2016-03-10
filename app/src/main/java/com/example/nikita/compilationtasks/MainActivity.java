94b4deb548fcc826a5f3083a1a3ad71b4ebae29b
Aqcee <nikitashtankowebdes@gmail.com>
2016-03-10 20:30:54 +0300

    Добавил шрифт, поправил разделы для каждого эл-та.

--- app/src/main/java/com/example/nikita/compilationtasks/MainActivity.java
+++ app/src/main/java/com/example/nikita/compilationtasks/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.nikita.compilationtasks;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.editText1);
+        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/Bebas.ttf");
+        textView.setTypeface(typeFace);
+        TextView textView2 = (TextView)findViewById(R.id.editText2);
+        textView2.setTypeface(typeFace);
+        TextView textView3 = (TextView)findViewById(R.id.textView);
+        textView3.setTypeface(typeFace);
+        TextView textView4 = (TextView)findViewById(R.id.textView2);
+        textView4.setTypeface(typeFace);
     }
 }
