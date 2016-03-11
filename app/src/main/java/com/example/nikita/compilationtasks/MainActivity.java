3c4ea1f40e5d0453f326947e0aeb8c31170cb073
Aqcee <nikitashtankowebdes@gmail.com>
2016-03-11 17:56:05 +0300

    Добавил шрифт "Пт санс" для текста. Настроил скроллинг в окне с вопросом. В Java файле закоминтена неработающая функция

--- app/src/main/java/com/example/nikita/compilationtasks/MainActivity.java
+++ app/src/main/java/com/example/nikita/compilationtasks/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.nikita.compilationtasks;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,15 +14,26 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Определение нужных шрифтов
+        Typeface Typeface1 = Typeface.createFromAsset(getAssets(), "fonts/Bebas.ttf");
+        Typeface Typeface2 = Typeface.createFromAsset(getAssets(), "fonts/PTSansRegular.ttf");
 
-        TextView textView = (TextView)findViewById(R.id.editText1);
-        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/Bebas.ttf");
-        textView.setTypeface(typeFace);
-        TextView textView2 = (TextView)findViewById(R.id.editText2);
-        textView2.setTypeface(typeFace);
-        TextView textView3 = (TextView)findViewById(R.id.textView);
-        textView3.setTypeface(typeFace);
-        TextView textView4 = (TextView)findViewById(R.id.textView2);
-        textView4.setTypeface(typeFace);
+        // Задача их для нужных элементов
+        TextView textView = (TextView)findViewById(R.id.FirstButtonAns);
+        textView.setTypeface(Typeface1);
+        TextView textView2 = (TextView)findViewById(R.id.SecondButtonAns);
+        textView2.setTypeface(Typeface1);
+        TextView textView3 = (TextView)findViewById(R.id.category);
+        textView3.setTypeface(Typeface1);
+        TextView textView4 = (TextView)findViewById(R.id.precategory);
+        textView4.setTypeface(Typeface1);
+        TextView textView5 = (TextView)findViewById(R.id.question);
+        textView5.setTypeface(Typeface2);
+        // Изменение цвета/Или другие действия по нажатию кнопки
+
     }
+  //  LinearLayout myView = (LinearLayout)findViewById(R.id.clickbutton);
+   // public void clickbutton(View v) {
+   //     myView.setBackgroundResource(R.drawable.buttonpressed);
+    //}
 }
