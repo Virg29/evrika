e8db32653e0df37d7279d0d6e07e3b75a6a61ab8
Virg29 <denjik505@mail.ru>
2016-03-17 15:55:13 +0300

    переназвал файлики

---
+++ app/src/main/java/com/example/nikita/compilationtasks/Menu_Activity.java
@@ -0,0 +1,31 @@
+package com.example.nikita.compilationtasks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Menu_Activity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.menuactivity);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
