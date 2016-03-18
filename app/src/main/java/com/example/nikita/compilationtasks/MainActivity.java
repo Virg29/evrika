c4514474a17fb1b974c554bd6744a5fd114a5981
Virg29 <denjik505@mail.ru>
2016-03-19 00:28:49 +0300

    this demonstration version make by me with fire from my ass,error that names nullPointerExpection not bugging me over 2 hours

--- app/src/main/java/com/example/nikita/compilationtasks/MainActivity.java
+++ app/src/main/java/com/example/nikita/compilationtasks/MainActivity.java
@@ -18,6 +18,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //это потомучто мы любим жарить дерьмо
+    int page = 1;//собственно объяснять не буду
+    //это кароче я хз как назыается,но мы попрежнему любим жарить говно!!1жькдрзхк
+
+    //продолжаем тему "база данных для слабаков"
+
+   //говно необходимо прожаривать со всех сторон а nullPointerExpection подвигает людей к этому
+
+    int[] bcate = new int[]{R.string.otvet1_1,R.string.otvet1_2,R.string.otvet1_3,R.string.otvet1_4,R.string.otvet1_5,R.string.otvet2_1,R.string.otvet2_2,R.string.otvet2_3,R.string.otvet2_4,R.string.otvet2_5,R.string.otvet3_1,R.string.otvet3_2,R.string.otvet3_3,R.string.otvet3_4,R.string.otvet3_5,R.string.otvet4_1,R.string.otvet4_2,R.string.otvet4_3,R.string.otvet4_4,R.string.otvet4_5};
+    int otvet = bcate[0];
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,42 +56,26 @@ public class MainActivity extends AppCompatActivity {
         //кнопки
         LinearLayout firstbutton = (LinearLayout)findViewById(R.id.clickbutton);
         LinearLayout secondbutton = (LinearLayout)findViewById(R.id.clickbutton2);
-        //это кароче я хз как назыается
+
+
+        //потом убрать
+        LinearLayout thirstbutton = (LinearLayout)findViewById(R.id.namequetion);
+        //вот что сверху
+
+
+
         Intent intent = getIntent();
-        int type = intent.getIntExtra("type",0);
-        int page = 1;
+        int type = intent.getIntExtra("type", 0);
 
 
-        if (type == 1){
-        //запуск некой функции которая будет распозновать жест и создавать новый лейаут уже с другим текстом посмотришь его в strings.xml там все разбито и потои разберемся
-            textView5.setText(R.string.lvl1_coplition1);
-
-
-        }
-        else if (type == 2) {
-            textView5.setText(R.string.lvl2_coplition1);
-
-
-
-
-
-        }
-        else if (type == 3) {
-            textView5.setText(R.string.lvl3_coplition1);
+        show();
 
 
 
 
 
 
-        }
-        else if (type == 4){
-            textView5.setText(R.string.lvl4_coplition1);
 
-
-
-
-        }
 
         //слушатели
         secondbutton.setOnTouchListener(new View.OnTouchListener() {
@@ -90,24 +87,38 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else  {
                     secondbutton.setBackgroundResource(R.drawable.buttonpressed2);
+
+
+
+                    if (page < 5){
+                        page++;                                                                                             //ЭТО ДЛЯ ДЕМОНСТРАЦИИ
+                    }
+                    show();
+
+
                 }
                 return false;
             };
         });
 
+
+
+
+
+
         firstbutton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 LinearLayout firstbutton = (LinearLayout) findViewById(R.id.clickbutton);
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    firstbutton.setBackgroundResource(R.drawable.buttonpressed);
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    firstbutton.setBackgroundResource(R.drawable.button);
                 } else {
-                    firstbutton.setBackgroundResource(R.drawable.answer);
+                    firstbutton.setBackgroundResource(R.drawable.buttonpressed2);
                     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("Подсказка!");
+                    builder.setTitle("А вот и ответ");
                     builder.setCancelable(false);
-                    builder.setMessage(R.string.lvl1_coplition1);
-                    builder.setNegativeButton("чта,как блядь?!", new DialogInterface.OnClickListener() {
+                    builder.setMessage(otvet);
+                    builder.setNegativeButton("Ок!", new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int id) {
                                     dialog.cancel();
                                 }
@@ -120,13 +131,169 @@ public class MainActivity extends AppCompatActivity {
 
             ;
         });
-        };
 
 
 
 
+
+        //НЕЗАБЫТЬ УБРАТЬ ИЛИ ОСТАВИТЬ
+//НЕЗАБЫТЬ УБРАТЬ ИЛИ ОСТАВИТЬ
+//НЕЗАБЫТЬ УБРАТЬ ИЛИ ОСТАВИТЬ
+//НЕЗАБЫТЬ УБРАТЬ ИЛИ ОСТАВИТЬ
+
+
+
+        thirstbutton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                LinearLayout secondbutton = (LinearLayout)findViewById(R.id.namequetion);
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    secondbutton.setBackgroundResource(R.drawable.button);
+                }
+                else if(event.getAction() == MotionEvent.ACTION_DOWN)  {
+                    secondbutton.setBackgroundResource(R.drawable.buttonpressed2);
+
+
+
+                    if (page > 1){
+                        page--;                                                                                             //ЭТО ДЛЯ ДЕМОНСТРАЦИИ
+                    }
+                    show();
+
+
+                }
+
+                return false;
+            };
+        });
+
+
+
+
+
+
+
+
+
+    };
+
+
+
+    public void show(){
+
+        Intent intent = getIntent();
+        int type = intent.getIntExtra("type", 0);
+        TextView textView5 = (TextView)findViewById(R.id.question);
+        switch (type){
+            case(1):
+                switch (page){
+                    case(1):
+                        textView5.setText(R.string.lvl1_coplition1);
+                        otvet = bcate[0] ;
+                        break;
+                    case(2):
+                        textView5.setText(R.string.lvl1_coplition2);
+                        otvet = bcate[1];
+                        break;
+                    case(3):
+                        textView5.setText(R.string.lvl1_coplition3);
+                        otvet = bcate[2];
+                        break;
+                    case(4):
+                        textView5.setText(R.string.lvl1_coplition4);
+                        otvet = bcate[3];
+                        break;
+                    case(5):
+                        textView5.setText(R.string.lvl1_coplition5);
+                        otvet = bcate[4];
+                        break;
+                }
+                break;
+            case(2):
+                switch (page){
+                    case(1):
+                        textView5.setText(R.string.lvl2_coplition1);
+                        otvet = bcate[5];
+                        break;
+                    case(2):
+                        textView5.setText(R.string.lvl2_coplition2);
+                        otvet = bcate[6];
+                        break;
+                    case(3):
+                        textView5.setText(R.string.lvl2_coplition3);
+                        otvet = bcate[7];
+                        break;
+                    case(4):
+                        textView5.setText(R.string.lvl2_coplition4);
+                        otvet = bcate[8];
+                        break;
+                    case(5):
+                        textView5.setText(R.string.lvl2_coplition5);
+                        otvet = bcate[9];
+                        break;
+                }
+                break;
+
+            case(3):
+                switch (page){
+                    case(1):
+                        textView5.setText(R.string.lvl3_coplition1);
+                        otvet = bcate[10];
+                        break;
+                    case(2):
+                        textView5.setText(R.string.lvl3_coplition2);
+                        otvet = bcate[11];
+                        break;
+                    case(3):
+                        textView5.setText(R.string.lvl3_coplition3);
+                        otvet = bcate[12];
+                        break;
+                    case(4):
+                        textView5.setText(R.string.lvl3_coplition4);
+                        otvet = bcate[13];
+                        break;
+                    case(5):
+                        textView5.setText(R.string.lvl3_coplition5);
+                        otvet = bcate[15];
+                        break;
+                }
+
+                break;
+
+            case(4):
+                switch (page){
+                    case(1):
+                        textView5.setText(R.string.lvl4_coplition1);
+                        otvet = bcate[16];
+                        break;
+                    case(2):
+                        textView5.setText(R.string.lvl4_coplition2);
+                        otvet = bcate[16];
+                        break;
+                    case(3):
+                        textView5.setText(R.string.lvl4_coplition3);
+                        otvet = bcate[17];
+                        break;
+                    case(4):
+                        textView5.setText(R.string.lvl4_coplition4);
+                        otvet = bcate[18];
+                        break;
+                    case(5):
+                        textView5.setText(R.string.lvl4_coplition5);
+                        otvet = bcate[19];
+                        break;
+                }
+
+                break;
+        }
+
+        //конец
 
 
     }
+
+
+
+}
 
 
